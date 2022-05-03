3c154e0b700593d45ee3afeb65f337a79f83dd10
nathanieladiah <nathanieladiah@gmail.com>
2022-05-03 17:18:15 -0400

    Complete mobile layout
    Changed font size map to allow for fonts on desktop and mobile to be
    accurate.
    
    Added javascript to allow mobile nave to be toggled, along with styles
    including an overlay to the body and stopping scrolling when mobile nav
    is open.
    
    Added the dropdown menus for the mobile nav and javascript to open them
    including animations for the sliding down and creating space.

---
+++ static/js/navigation.js
@@ -0,0 +1,50 @@
+const nav = document.getElementById('mobile-nav');
+const navToggle = document.getElementById('nav-toggle');
+const header = document.getElementById('main-header');
+const body = document.querySelector('body');
+
+navToggle.onclick = () => {
+	const visibility = nav.dataset.visible
+
+	if (visibility === "false") {
+		nav.dataset.visible = 'true';
+		navToggle.setAttribute('aria-expanded', true);
+		header.dataset.overlay = 'true';
+		body.classList.add('noscroll');
+
+	} else {
+		header.dataset.overlay = 'false';
+		nav.dataset.visible = 'false';
+		navToggle.setAttribute('aria-expanded', false);
+		body.classList.remove('noscroll');
+	}
+}
+
+// handle nav dropdowns
+
+const dropToggles = document.querySelectorAll('.nav-dropdown-toggle');
+
+dropToggles.forEach(toggle => {
+	toggle.onclick = () => {
+		const dropdownId = toggle.getAttribute('aria-controls');
+		const dropdown = document.getElementById(dropdownId);
+
+		const visibility = dropdown.dataset.visible;
+
+		if (visibility === "false") {
+			dropdown.dataset.visible = 'true';
+			toggle.setAttribute('aria-expanded', true);
+
+		} else {
+			dropdown.setAttribute('closing', '');
+
+			dropdown.addEventListener('animationend', () => {
+				dropdown.removeAttribute('closing');
+				dropdown.dataset.visible = 'false';
+				toggle.setAttribute('aria-expanded', false);
+			}, {once: true});
+		}
+
+		// console.log(dropdownId);
+	}
+})
